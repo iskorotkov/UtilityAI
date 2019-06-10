4e6ecc021a609c5e6920a0f4fb2e12b4da8677ef
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-06-10 16:05:37 +0500

    Add BrainAsset

---
+++ Source/UtilityAI/Public/BrainAsset.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "BrainAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType, Abstract)
+class UTILITYAI_API UBrainAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	virtual UAction* SelectAction() const = 0;
+
+private:
+	UPROPERTY(EditAnywhere)
+	TArray<UAction*> Actions;
+};
