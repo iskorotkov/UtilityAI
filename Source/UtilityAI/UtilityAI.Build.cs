143aa15b07a1b9d1bf22fca92df8b1421ec6dc8b
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-06-10 17:34:24 +0500

    Add AIModule to build file

--- Source/UtilityAI/UtilityAI.Build.cs
+++ Source/UtilityAI/UtilityAI.Build.cs
@@ -7,26 +7,12 @@ public class UtilityAI : ModuleRules
 	public UtilityAI(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-			
-		
-		PublicDependencyModuleNames.AddRange(
+
+        PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
 				"Core",
-				// ... add other public dependencies that you statically link with here ...
+                "AIModule",
 			}
 			);
 			
