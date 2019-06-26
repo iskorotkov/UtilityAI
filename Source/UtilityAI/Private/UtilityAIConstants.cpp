9f143223029485b2c964b0a475fb2b387060799f
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-06-26 17:48:09 +0500

    Add class with constants

---
+++ Source/UtilityAI/Private/UtilityAIConstants.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UtilityAIConstants.h"
+
+float UUtilityAIConstants::MinActionRating()
+{
+	return 0.0f;
+}
+
+float UUtilityAIConstants::MaxActionRating()
+{
+	return 1.0f;
+}
