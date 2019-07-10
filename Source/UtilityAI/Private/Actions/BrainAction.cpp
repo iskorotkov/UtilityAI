c982e7b811e00396b7519b6b4443854300ba9581
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-07-10 08:42:13 +0500

    Add BrainAction class

---
+++ Source/UtilityAI/Private/Actions/BrainAction.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BrainAction.h"
+#include "UtilityAI.h"
+#include "BrainAsset.h"
+
+void UBrainAction::Run_Implementation(const TScriptInterface<IAgent>& Agent) const
+{
+	Super::Run(Agent);
+	if (Brain == nullptr)
+	{
+		UE_LOG(UtilityAI_Actions, Warning, TEXT("Action %s has no associated brain asset"), *GetName());
+		return;
+	}
+	if (const auto Action = Brain->SelectAction(Agent))
+	{
+		Action->Run(Agent);
+	}
+}
---
+++ Source/UtilityAI/Public/Actions/BrainAction.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actions/Action.h"
+#include "BrainAction.generated.h"
+
+class UBrainAsset;
+/**
+ * 
+ */
+UCLASS()
+class UTILITYAI_API UBrainAction : public UAction
+{
+	GENERATED_BODY()
+
+public:
+	void Run_Implementation(const TScriptInterface<IAgent>& Agent) const override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	UBrainAsset* Brain;
+};
